f3898f5b8d7d9f6026a5f209b710932a21944444
dko <dko@nv.local>
2019-12-05 11:21:24 +0100

    Bug 2019120851: PBE blockiert FSConsole Log

---
+++ src/PBE/Utils/ParallelHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+
+namespace PBE.Utils
+{
+    public class ParallelHelper
+    {
+        private static readonly object localMutex = new object();
+
+        /// <summary>
+        /// Liest den Inhalt der Datei ein. Dabei wird der Zugriff über einen Mutex abgesichert.
+        /// Es ist das Gegenstück zu ParallelHelper.FileAppendAllText() im FS
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        public static string FileReadAllText(string filename)
+        {
+            lock (localMutex)
+            {
+                string mutexName = Path.GetFullPath(filename).ToLowerInvariant();
+                using (FSUtils.GetMutex(mutexName))
+                {
+                    return File.ReadAllText(filename);
+                }
+            }
+        }
+    }
+}
